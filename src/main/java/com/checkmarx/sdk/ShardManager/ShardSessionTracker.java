374b8576be80d2bc1ef8b1b0bbaa3205bec6af69
Jeff Armstrong <jarmstrong@nezasoft.com>
2020-09-09 16:21:58 -0700

    Added mock ShardManager classes
    CxFlow requires stubbed classes for ScanRequestConverter.java to compile correctly.

---
+++ src/main/java/com/checkmarx/sdk/ShardManager/ShardSessionTracker.java
@@ -0,0 +1,14 @@
+package com.checkmarx.sdk.ShardManager;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ShardSessionTracker {
+
+    //
+    /// This doesn't exist in the CxGO world.
+    //
+    public ShardSession getShardSession() {
+        return null;
+    }
+}
+
