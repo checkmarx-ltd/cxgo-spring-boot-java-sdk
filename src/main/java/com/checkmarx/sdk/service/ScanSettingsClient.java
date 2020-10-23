b5179f63362749bc8efe72334d220df60fce4f37
Jeff Armstrong <jarmstrong@nezasoft.com>
2020-10-22 23:07:47 -0700

    Fixed missing bean
    Missing ScanSettingClient bean.

--- src/main/java/com/checkmarx/sdk/service/ScanSettingsClient.java
+++ src/main/java/com/checkmarx/sdk/service/ScanSettingsClient.java
@@ -1,27 +1,14 @@
 package com.checkmarx.sdk.service;
 
-import com.checkmarx.sdk.dto.cx.CxScanSettings;
-import com.checkmarx.sdk.exception.CheckmarxException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 /**
  * Works with scan settings. The settings contain<br>
  * - engine configuration<br>
  * - scan presets
  */
-public interface ScanSettingsClient {
-    int createScanSettings(int projectId, int presetId, int engineConfigId, int postScanId);
-
-    String getScanSettings(int projectId);
-
-    CxScanSettings getScanSettingsDto(int projectId);
-
-    int getPresetId(String preset) throws CheckmarxException;
-
-    String getPresetName(int presetId);
-
-    int getProjectPresetId(int projectId);
-
-    int getEngineConfigurationId(String configurationName) throws CheckmarxException;
-
-    String getEngineConfigurationName(int configurationId);
+@Service
+@Slf4j
+public class ScanSettingsClient {
 }
